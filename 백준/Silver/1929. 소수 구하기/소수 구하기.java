92192f8392bd7379c1ec3342074b6d109bd672a0
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-09-18 00:20:35 +0900

    Update 소수 구하기.java

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,18 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException{
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		int m = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
 
-		boolean array[] = new boolean[n + 1];
+		boolean[] array = new boolean[n + 1];
 		array[0] = array[1] = true;
 
 		// 에라토스테네스의 체
@@ -21,17 +20,14 @@ public class Main {
 		int sqrt = (int) Math.sqrt(n);
 		for (int i = 2; i <= sqrt; i ++) {
 			for (int j = 2; j <= (n / i); j++) {
-				if (array[i * j] == true) {
-					continue;
-				}
-				else {
+				if (!array[i * j]) {
 					array[i * j] = true;
 				}
 			}
 		}
-        
+
 		for (int i = m; i <= n ; i++){
-			if (array[i] == false) {
+			if (!array[i]) {
 				System.out.println(i);
 			}
 		}
