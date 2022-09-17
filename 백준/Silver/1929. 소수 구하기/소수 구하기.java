65fe26ed20eab1788224b4854077b6744e10799d
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-09-18 00:14:01 +0900

    [Silver III] Title: 소수 구하기, Time: 1060 ms, Memory: 35984 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+
+		boolean array[] = new boolean[n + 1];
+		array[0] = array[1] = true;
+
+		// 에라토스테네스의 체
+		// 2부터 시작, 소수의  제곱과 배수들을 제외한다.
+		// ex) 2는 소수 -> 4, 6, 8, 10 ... 제외
+		int sqrt = (int) Math.sqrt(n);
+		for (int i = 2; i <= sqrt; i ++) {
+			for (int j = 2; j <= (n / i); j++) {
+				if (array[i * j] == true) {
+					continue;
+				}
+				else {
+					array[i * j] = true;
+				}
+			}
+		}
+        
+		for (int i = m; i <= n ; i++){
+			if (array[i] == false) {
+				System.out.println(i);
+			}
+		}
+	}
+}
