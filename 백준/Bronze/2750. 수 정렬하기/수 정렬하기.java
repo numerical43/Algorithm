88c80edfb9ac444896d80a93eba4938ed727ef09
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-09-22 00:59:40 +0900

    [Bronze II] Title: 수 정렬하기, Time: 172 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] nums = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+}
