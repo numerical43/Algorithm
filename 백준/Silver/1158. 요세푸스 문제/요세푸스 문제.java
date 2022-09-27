c46ef7cd08b71a20446168f1bf6c1fa500c2e003
numerical43 <61495627+numerical43@users.noreply.github.com>
2022-09-28 00:35:03 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 568 ms, Memory: 294792 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		Queue<Integer> queue = new LinkedList<>();
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= n; i++) {
+			queue.offer(i);
+		}
+
+		sb.append("<");
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < (k - 1); j++) {
+				queue.offer(queue.poll());
+			}
+			sb.append(queue.poll()).append(", ");
+		}
+		//맨 뒤의 ', ' 삭제
+		sb.setLength(sb.length() - 2);
+		sb.append(">");
+
+		System.out.println(sb);
+	}
+}
