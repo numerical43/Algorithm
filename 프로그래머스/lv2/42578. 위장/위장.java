3c2fc70ae6b761c99b3c983ff998d83105459953
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-01-27 15:06:30 +0900

    [level 2] Title: 위장, Time: 0.13 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -7,14 +7,14 @@ class Solution {
         HashMap<String, Integer> hash = new HashMap<>();
 
         for (String[] cloth : clothes)
-            hash.merge(cloth[1], 1, (a, b) -> hash.get(cloth[b]) + b);
+            hash.put(cloth[1], hash.getOrDefault(cloth[1], 0) + 1);
 
         for (String cloth : hash.keySet())
             answer *= (hash.get(cloth) + 1);
 
         // 어느 옷도 선택하지 않는 경우
         answer--;
-        
+
         return answer;
     }
 }
