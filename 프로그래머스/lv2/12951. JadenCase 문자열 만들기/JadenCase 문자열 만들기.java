dffd13e3821f49338334a7bf5917dc7adf876683
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-01-30 16:09:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.17 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,38 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 class Solution {
     public String solution(String s) {
-        String answer;
-        List<String> list = new ArrayList<>();
+        StringBuilder answer = new StringBuilder();
+        String[] temp = s.toLowerCase().split("");
         boolean flag = true;
 
-        for (int i = 0; i < s.length(); i++)
-            list.add(String.valueOf(s.charAt(i)).toLowerCase(Locale.ROOT));
-
-        list.set(0, list.get(0).toUpperCase(Locale.ROOT));
-
-        for (int i = 0; i < s.length(); i++) {
-            if (flag && !list.get(i).equals(" "))
-                continue;
-            else if (list.get(i).equals(" ")) {
-                flag = false;
-                continue;
-            }
-
-            if (!flag && list.get(i - 1).equals(" ")) {
-                list.set(i, list.get(i).toUpperCase(Locale.ROOT));
-                flag = true;
-            }
+        for (String now : temp) {
+            answer.append(flag ? now.toUpperCase() : now);
+            flag = now.equals(" ") ? true : false;
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (String value : list)
-            sb.append(value);
-
-        answer = sb.toString();
-
-        return answer;
+        
+        return answer.toString();
     }
 }
