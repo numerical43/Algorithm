c7195fca501397a7e3f22ceeb76b068c9ff7a19d
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-01-30 16:25:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,14 +1,35 @@
-class Solution {
-  public String solution(String s) {
-        String answer = "";
-        String[] sp = s.toLowerCase().split("");
-        boolean flag = true;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-        for(String ss : sp) {
-            answer += flag ? ss.toUpperCase() : ss;
-            flag = ss.equals(" ") ? true : false;
+class Solution {
+    public String solution(String s) {
+        List<String> list = new ArrayList<>();
+        boolean flag = false;
+
+        // 소문자로 전부 list 에 저장
+        for (int i = 0; i < s.length(); i++)
+            list.add(String.valueOf(s.charAt(i)).toLowerCase(Locale.ROOT));
+
+        for (int i = 0; i < s.length(); i++) {
+            // 한 단어의 첫 문자가 아닌 문자들 건너뛰기
+            if (flag && !list.get(i).equals(" "))
+                continue;
+                // 공백이라면 flag 초기화
+            else if (list.get(i).equals(" ")) {
+                flag = false;
+                continue;
+            }
+
+            // 단어의 첫 문자 대문자로 넣고 flag = true 로
+            list.set(i, list.get(i).toUpperCase());
+            flag = true;
         }
 
-        return answer;
-  }
+        StringBuilder sb = new StringBuilder();
+        for (String value : list)
+            sb.append(value);
+
+        return sb.toString();
+    }
 }
