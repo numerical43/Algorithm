3755c7c9c85242d0d5e6f0b0a0b8c55bfdababcb
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-02-16 16:52:40 +0900

    [Silver IV] Title: 돌 게임 2, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -6,11 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        if ((n % 2) == 0)
-            System.out.println("SK");
-        else
-            System.out.println("CY");
+        
+        System.out.println((n % 2 == 0) ? "SK" : "CY");
     }
-
 }
