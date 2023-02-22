5756be417361ef8dc9e202b5b116e828543e7e17
sui <numerical43@daum.net>
2023-02-22 15:18:17 +0900

    [Silver III] Title: 소수 구하기, Time: 1060 ms, Memory: 35984 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,16 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException{
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		int m = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
-
 		boolean[] array = new boolean[n + 1];
 		array[0] = array[1] = true;
 
