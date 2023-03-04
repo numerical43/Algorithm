de32a060854e03580430619a91477afdc3203eef
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-03-04 14:46:39 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 18628 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main (String[] args){
+        Date today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(today));
+    }
+}
