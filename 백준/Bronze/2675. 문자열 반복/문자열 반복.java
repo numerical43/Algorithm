1d45f4be6abbe371213b95588073a2319c41acb7
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-04-17 15:45:19 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			int r = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+
+			for (int j = 0; j < s.length(); j++){
+				sb.append(String.valueOf(s.charAt(j)).repeat(Math.max(0, r)));
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
