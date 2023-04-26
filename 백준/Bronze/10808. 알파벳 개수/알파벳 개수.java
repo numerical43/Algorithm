33a77b3d89a6ed02607bedd19eac489a68fe1914
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-04-26 23:58:02 +0000

    [Bronze IV] Title: 알파벳 개수, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] count = new int[26];
+
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++)
+            count[(int) input.charAt(i) - 97] += 1;
+
+        for (int j : count)
+            sb.append(j).append(" ");
+
+        System.out.println(sb);
+    }
+}
