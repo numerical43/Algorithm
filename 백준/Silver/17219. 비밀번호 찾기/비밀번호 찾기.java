73c293b7e1e4421b092b60866a7d0825edf2e720
numerical43 <61495627+numerical43@users.noreply.github.com>
2023-05-29 10:08:45 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 624 ms, Memory: 55232 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Map<String, String> hashMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String address = st.nextToken();
+            String password = st.nextToken();
+
+            hashMap.put(address, password);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            String find = br.readLine();
+            sb.append(hashMap.get(find)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
