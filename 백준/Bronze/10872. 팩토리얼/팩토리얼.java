6c9be4ad1f3ace3642f9cfce8861037a29aa6894
SUI <numerical43@gmail.com>
2024-01-30 19:29:57 +0000

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,20 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+        
+		int sum = factorial(n);
+        
+		System.out.println(sum);
+	}
+	
+	public static int factorial(int n) {
+		if (n <= 1) 
+            return 1;
+        
+		return (n * factorial(n - 1));		
+	}
+}
+ 
